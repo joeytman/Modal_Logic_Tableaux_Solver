b1196c1caff336c0aeb793e7c04f8ce4a9fbde04
joeytman <jhthaidigsman@gmail.com>
2018-05-08 14:57:12 -0700

    wrote implementation, no testing yet

--- modalgraph.py
+++ modalgraph.py
@@ -61,6 +61,14 @@ class ModalGraph():
 		false_at_world[world].add(atom)
 		return True
 
+	""" Returns true if all formulas in all worlds have been processed and have had values assigned
+	"""
+	def is_fully_processed(self):
+		return False not in [True for item in self.formulas if len(item)==0 else False]
+
+	def is_consistent(self):
+		return [atom not in [self.false_at_world[world] for world in range(self.next_world_number)] for atom in [self.true_at_world[world] for world in range(self.next_world_number)]]
+
 
 	""" When called, processes all subformulas in all worlds until a new graph (or graphs) must be created
 		When new graph(s) are created, the function returns a list of all graphs within its scope that need processing
@@ -68,34 +76,34 @@ class ModalGraph():
 		If it did not create any new graphs within this call, that will be an empty list
 	"""
 	def process_all_worlds(self):
-		valid_graphs = [self]
+		active_graphs = [self]
 		for world, world_formulas in self.formulas:
 			for subformula in world_formulas:
 				if isinstance(subformula, str): 
 					# subformula at world is reduced to an atomic proposition that must be set to true
 					graph_still_valid = self.set_atom_true(world, subformula)
 					world_formulas.remove(subformula)
-					if not graph_still_valid and self in valid_graphs:
-						valid_graphs.remove(self)
+					if not graph_still_valid and self in active_graphs:
+						active_graphs.remove(self)
 				
 				elif isinstance(subformula, tuple): #Operation(s) to process still
 					operator = subformula[0]
-					if operator == UnaryPred.NOT:
-						action = self.handle_negation(subformula, world, world_formulas)
-						action[0](self, world, world_formulas, subformula, valid_graphs, action[1])
-					elif operator == UnaryPred.BOX:
-						action = 
-
-					elif operator == UnaryPred.DIAM:
-
-					elif operator == BinaryPred.AND:
-
-					elif operator == BinaryPred.OR:
-
-					elif operator == BinaryPred.IMPL:
-
+					if operator == UnaryPred.NOT: action = self.handle_negation(subformula, world, world_formulas)
+					elif operator == UnaryPred.BOX: action = self.handle_box(subformula, world, world_formulas)
+					elif operator == UnaryPred.DIAM: action = self.handle_diamond(subformula, world, world_formulas)
+					elif operator == BinaryPred.AND: action = self.handle_and(subformula, world, world_formulas)
+					elif operator == BinaryPred.OR: action = self.handle_or(subformula, world, world_formulas)
+					elif operator == BinaryPred.IMPL: action = self.handle_implication(subformula, world, world_formulas)
 					else: raise ValueError("Operator " + str(operator) + " is not of a known type")
-		return valid_graphs
+					action[0](self, world, world_formulas, subformula, active_graphs, action[1])
+
+				else: 
+					raise ValueError("Subformula " + str(subformula) + " is not of a known type")
+			
+			if self not in active_graphs: 
+				return active_graphs
+		if self.is_fully_processed(): active_graphs.remove(self)
+		return active_graphs
 
 
 	def handle_negation(self, subformula, world):
@@ -115,7 +123,7 @@ class ModalGraph():
 			elif nextop == BinaryPred.AND:
 				return (split_formula_over_new_graph, [(UnaryPred.NOT, arg[1]), (UnaryPred.NOT, arg[2])])
 			elif nextop == UnaryPred.NOT:
-				return (set_current_subformula, [arg[1]])
+				return (replace_current_subformula, [arg[1]])
 			elif nextop == UnaryPred.BOX:
 				return (add_new_world_with_subformula, [(UnaryPred.NOT, arg[1])])
 			elif nextop == UnaryPred.DIAM:
@@ -124,40 +132,61 @@ class ModalGraph():
 	def handle_box(self, subformula, world):
 		return (enforce_formula_met_by_children, [subformula[1]])
 
+	def handle_diamond(self, subformula, world):
+		return (add_new_world_with_subformula, [subformula[1]])
+
+	def handle_and(self, subformula, world):
+		phi = subformula[1]
+		psi = subformula[2]
+		return (split_subformula_in_world, [phi, psi])
+
+	def handle_or(self, subformula, world):
+		phi = subformula[1]
+		psi = subformula[2]
+		return (split_formula_over_new_graph, [phi, psi])
+
+	def handle_implication(self, subformula, world):
+		phi = (UnaryPred.NOT, subformula[1])
+		psi = subformula[2]
+		return (split_formula_over_new_graph, [phi, psi])
+
 	def __getitem__(self, world):
 		return self.nodes[world]
 
 
-def invalidate_graph(MG, world, world_formulas, subformula, valid_graphs, args):
-	if MG in valid_graphs:
-		valid_graphs.remove(MG)
+def invalidate_graph(MG, world, world_formulas, subformula, active_graphs, args):
+	if MG in active_graphs:
+		active_graphs.remove(MG)
 
-def finished_subformula(MG, world, world_formulas, subformula, valid_graphs, args):
+def finished_subformula(MG, world, world_formulas, subformula, active_graphs, args):
 	world_formulas.remove(subformula)
 
-def split_subformula_in_world(MG, world, world_formulas, subformula, valid_graphs, args):
+def split_subformula_in_world(MG, world, world_formulas, subformula, active_graphs, args):
 	world_formulas.remove(subformula)
 	world_formulas.add(args[0])
 	world_formulas.add(args[1])
 
-def split_formula_over_new_graph(MG, world, world_formulas, subformula, valid_graphs, args):
+def split_formula_over_new_graph(MG, world, world_formulas, subformula, active_graphs, args):
 	world_formulas.remove(subformula)
 	newMG = ModalGraph(MG=MG)
 	world_formulas.add(args[0])
 	newMG.formulas[world].add(args[1])
+	active_graphs.add(newMG)
 
-def set_current_subformula(MG, world, world_formulas, subformula, valid_graphs, args):
+def replace_current_subformula(MG, world, world_formulas, subformula, active_graphs, args):
 	world_formulas.remove(subformula)
 	world_formulas.add(args[0])
 
-def add_new_world_with_subformula(MG, world, world_formulas, subformula, valid_graphs, args):
+def add_new_world_with_subformula(MG, world, world_formulas, subformula, active_graphs, args):
 	newworld = MG.add_node(args[0])
 	MG.formulas[newworld].extend(MG.rules_for_children[world])
 	MG.nxG.add_edge(world, newworld)
+	world_formulas.remove(subformula) # Satisfied formula by creating child that satisfies it, remove requirement from this world
 
 
-def enforce_formula_met_by_children(MG, world, world_formulas, subformula, valid_graphs, args):
+def enforce_formula_met_by_children(MG, world, world_formulas, subformula, active_graphs, args):
 	MG.rules_for_children[world].add(args[0])
+	world_formulas.remove(subformula) # Transfer unprocessed subformula to processed child enforcement rule
 	for child in [edge[1] for edge in MG.out_edges_iter(world)]:
 		MG.formulas[child].add(args[0])
 
--- solver.py
+++ solver.py
@@ -12,12 +12,24 @@ if __name == '__main__':
 	args = parser.parse_args()
 	formula = args.formula
 	parsed_formula = modalparser.parse_formula()
+	valid_solution_graphs = perform_graph_tableaux(parsed_formula)
 
 """ Input: parsed formula in tuple prefix form
 	Each graph world 
 """
 def perform_graph_tableaux(parsed_formula):
 	MG = ModalGraph()
-	active_graphs = {MG}
-	while active_graphs and 
+	MG.add_node(parsed_formula)
+	active_graphs = [MG]
+	seen_graphs = [MG]
+	while active_graphs:
+		new_active_graphs = []
+		new_active_graphs.extend(MG.process_all_worlds())
+		seen_graphs += [new_graph for new_graph in new_active_graphs if new_graph not in seen_graphs]
+		active_graphs = new_active_graphs
+	valid_solution_graphs = []
+	for graph in seen_graphs:
+		if graph.is_fully_processed() and graph.is_consistent():
+			valid_solution_graphs.add(graph)
+	return valid_solution_graphs
 
